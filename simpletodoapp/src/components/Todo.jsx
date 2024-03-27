8610fdf9d8e669b4db5a6bf2c64ba241643e0fb1
KeijiPlata <lordmiackykeijip@gmail.com>
2024-03-27 16:58:33 +0800

    feat: store user input into todo's array

--- simpletodoapp/src/components/Todo.jsx
+++ simpletodoapp/src/components/Todo.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 
 export default function Todo() {
-  const [todo, SetTodo] = useState ("");
+  // get the user input
+  const [todo, setTodo] = useState ("");
+
+  // store to do items here
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e){
+    // this will prevent site from refreshing
+    e.preventDefault();
+
+    // using spread operator, create new array with the new input
+    setTodos([...todos, todo]);
+
+    // empty the input when submit
+    setTodo("");
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          onChange={(e) => SetTodo(e.target.value)}
+          onChange={(e) => setTodo(e.target.value)}
           value={todo}
         />
         <button type="submit">Add</button>
       </form>
+      {console.log(todos)}
     </div>
   );
 }
