6f829af1faee4f6e3da7542c22adf0efe9e59d79
KeijiPlata <lordmiackykeijip@gmail.com>
2024-03-27 13:11:13 +0800

    feat: add input text and submit btn

--- simpletodoapp/src/components/Todo.jsx
+++ simpletodoapp/src/components/Todo.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo() {
+  const [todo, SetTodo] = useState ("");
   return (
     <div>
-      <form action="">
-        <input type="text" name="" id="" />
+      <form>
+        <input
+          type="text"
+          onChange={(e) => SetTodo(e.target.value)}
+          value={todo}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
