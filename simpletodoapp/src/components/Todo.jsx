9af8a3168dcc2de765771deb0e701a4131a816ff
KeijiPlata <lordmiackykeijip@gmail.com>
2024-03-27 12:40:52 +0800

    chore: create components folder and install files

---
+++ simpletodoapp/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="" id="" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
