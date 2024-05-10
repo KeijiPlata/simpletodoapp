f48e5c36b7fc38a1d007d107f7eaf1859dfa67f5
KeijiPlata <lordmiackykeijip@gmail.com>
2024-05-11 02:24:08 +0800

    feat: designing the elements of list

--- simpletodoapp/src/components/ListToDo.jsx
+++ simpletodoapp/src/components/ListToDo.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 import TodoItem from "./TodoItem";
 
 function ListToDo({ todos }) {
   return (
-    <div>
-        {/* display todos using map */}
-        {todos.map((item) => (
-        <TodoItem key={item} item={item} />
+    <div className=" lg:w-5/6 md:w-10/12 w-full justify-center items-center">
+      {/* display todos using map */}
+      <h2 className="text-white text-4xl py-2">To Do</h2>
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-2">
+      {todos.map((item) => (
+        <div className="bg-customBlue  w-full text-center rounded-md p-5">
+          <TodoItem key={item} item={item} />
+        </div>
       ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ListToDo
+export default ListToDo;
