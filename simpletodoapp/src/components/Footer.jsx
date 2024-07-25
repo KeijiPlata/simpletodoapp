ab543b2cf7174991cb38098007371a4107c6a048
KeijiPlata <lordmiackykeijip@gmail.com>
2024-07-25 22:46:49 +0800

    feat: change of design, remove footer

--- simpletodoapp/src/components/Footer.jsx
+++ simpletodoapp/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer({ completedTodos, totalTodos }) {
   return (
-    <div className="text-white"> 
+    <div className="text-white flex md:flex-row md:gap-5 flex-col"> 
       <h1>Completed Todos: {completedTodos}</h1>
       <h2>Total Todos: {totalTodos}</h2>
     </div>
