498a223221c8c63ff3fabb3dba9904dcd40c1975
KeijiPlata <lordmiackykeijip@gmail.com>
2025-01-29 20:48:09 +0800

    chore: connect to atlas mongodb

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// All of the sensitive data is placed in .env file, use gitignore later
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connection to mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+// Routes
+app.get("/", (req, res) => res.send("API is running"));
+
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}!!`)
+});
