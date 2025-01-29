0c5c9c40098c9beb7e69b45f9a2a987a061883a2
KeijiPlata <lordmiackykeijip@gmail.com>
2025-01-30 02:44:37 +0800

    feat: edit and delete to database

--- simpletodoapp/src/components/TodoItem.jsx
+++ simpletodoapp/src/components/TodoItem.jsx
@@ -1,34 +1,50 @@
 import React from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FaCheck } from "react-icons/fa6";
+import axios from "axios";
 
 export default function TodoItem({ item, todos, setTodos }) {
-  // filters, if the element todo is equal to item, it will not store it to the array
-  function handleDelete() {
-    setTodos(todos.filter((todo) => todo.id !== item.id));
+   const API_URL = "http://localhost:4000/api/todos"; 
+
+  // Delete a todo item from the database
+  async function handleDelete() {
+    try {
+      await axios.delete(`${API_URL}/${item._id}`); 
+      setTodos(todos.filter((todo) => todo._id !== item._id)); 
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
   }
-  function handleClick(id) {
-    // this will create new todo array
-    // this checks if the todo.name is equal to name, if that's the case, make todo.done into something else
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo
-      )
-    );
+ 
+  // Toggle the 'done' status in the database
+  async function handleClick() {
+    try {
+      const response = await axios.put(`${API_URL}/${item._id}`, {
+        done: !item.done, 
+      });
+
+      setTodos(
+        todos.map((todo) =>
+          todo._id === item._id ? { ...todo, done: response.data.done } : todo
+        )
+      );
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
   }
+
   return (
-    <div className="flex flex-row justify-between items-center">
+    <div className="flex flex-row items-center justify-between">
       <div>
         <h3
           className={`lg:text-2xl md:text-xl text-lg text-start ${item.done ? "line-through" : "no-underline"}`}
         >
           {item.name}
         </h3>
-        <h3 className="text-start text-sm">{item.date}</h3>
+        <h3 className="text-sm text-start">{item.date}</h3>
       </div>
       <div className="flex flex-row gap-2">
         <div className="text-3xl cursor-pointer">
-          {/* call back function will not immediately trigger the function */}
           <FaCheck onClick={() => handleClick(item.id)} />
         </div>
         <div className="text-3xl cursor-pointer">
