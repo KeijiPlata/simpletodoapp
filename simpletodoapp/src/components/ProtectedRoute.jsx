0afca8463c3436fb62c63deb6f44bbfe3339e3cb
KeijiPlata <lordmiackykeijip@gmail.com>
2025-02-01 19:44:42 +0800

    feat: add design for the loading, redirect the site to login if no user

--- simpletodoapp/src/components/ProtectedRoute.jsx
+++ simpletodoapp/src/components/ProtectedRoute.jsx
@@ -1,13 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { FaSpinner } from "react-icons/fa6";
 import AuthContext from "../context/AuthContext";
 
 function ProtectedRoute({ element }) {
   const { user } = useContext(AuthContext);
+  const [checkingUser, setCheckingUser] = useState(true);
 
-  // Avoid redirecting before user state is properly restored
-  if (user === null) {
-    return <div>Loading...</div>; // Temporary loading state
+  useEffect(() => {
+    // if there is no user for 1 second, redirect to login
+    const timer = setTimeout(() => {
+      setCheckingUser(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (checkingUser) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-screen gap-5 text-white">
+        <div className="text-5xl animate-spin">
+          <FaSpinner />
+        </div>
+        <h1 className="text-3xl">Loading ...</h1>
+      </div>
+    );
   }
 
   return user ? element : <Navigate to="/login" replace />;
